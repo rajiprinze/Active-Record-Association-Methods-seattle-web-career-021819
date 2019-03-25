dc802d28ba000f8867ce86026e50c9e81566cc12
Soundarya Rajendran <soundaryarj5@gmail.com>
2019-03-25 15:03:35 -0700

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -4,13 +4,23 @@ class Artist < ActiveRecord::Base
 
   def get_genre_of_first_song
     #return the genre of the artist's first saved song
+    # binding.pry
+    self.songs.first.genre
+    # Genre.where(:id => self.songs.find(self).genre_id).to_a[0][:name]
   end
 
   def song_count
     #return the number of songs associated with the artist
+    self.songs.count
   end
 
   def genre_count
     #return the number of genres associated with the artist
+    # binding.pry
+    self.genres.count
+  end
+
+  def get_first_song
+    self.songs.first
   end
 end
